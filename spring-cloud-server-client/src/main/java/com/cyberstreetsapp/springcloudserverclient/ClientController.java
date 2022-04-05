f0f368a31e4e52dd696d30f60849b36031f21217
stolido-cap <102696307+stolido-cap@users.noreply.github.com>
2022-04-05 18:57:44 -0400

    Add files via upload

--- spring-cloud-server-client/src/main/java/com/cyberstreetsapp/springcloudserverclient/ClientController.java
+++ spring-cloud-server-client/src/main/java/com/cyberstreetsapp/springcloudserverclient/ClientController.java
@@ -1,14 +1,54 @@
 package com.cyberstreetsapp.springcloudserverclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+
+@RefreshScope
+@RequestMapping("/run")
+@RestController
 public class ClientController {
 
+
+	//template used to read data off page
+	private RestTemplate template;
 	
-	public static void main(String[] args) {
-		SpringApplication.run(ClientController.class, args);
+	@Value("${message: The server is running, but not working}")
+	private String message;
+	
+	@GetMapping("/test")
+	public String message() {
+		return message;
 	}
+	//@getMapping("/tag to assign page")
+	//Create call to webpage
+	//public ...
+	
+	//@getMapping("/tag to assign page")
+		//Create call to webpage
+		//public ...
+	
+	//@getMapping("/tag to assign page")
+		//Create call to webpage
+		//public ...
+	
+
+	
+	
+	//initialized the template above
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
+	}
+	
+	
+	
 	
 }
--- spring-cloud-server-client/src/main/java/com/cyberstreetsapp/springcloudserverclient/SpringCloudServerClientApplication.java
+++ spring-cloud-server-client/src/main/java/com/cyberstreetsapp/springcloudserverclient/SpringCloudServerClientApplication.java
@@ -11,40 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RefreshScope
-@RequestMapping("/run")
-@RestController
+@SpringBootApplication
 public class SpringCloudServerClientApplication {
 
-	//template used to read data off page
-	private RestTemplate template;
-	
-	@Value("${message: The server is running, but not working}")
-	private String message;
-	
-	@GetMapping("/test")
-	public String message() {
-		return message;
-	}
-	//@getMapping("/tag to assign page")
-	//Create call to webpage
-	//public ...
-	
-	//@getMapping("/tag to assign page")
-		//Create call to webpage
-		//public ...
-	
-	//@getMapping("/tag to assign page")
-		//Create call to webpage
-		//public ...
-	
-
-	
-	
-	//initialized the template above
-	@Bean
-	public RestTemplate template() {
-		return new RestTemplate();
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerClientApplication.class, args);
 	}
 
 }
